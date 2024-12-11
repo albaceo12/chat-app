97b571c48408f1667d97f829c6c514b5e010f92a
albaceo12 <h.nouri1999@gmail.com>
2024-12-11 23:01:20 +0330

    final stage completed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/chat-app-realtime",
+  // base: "/chat-app-realtime",
   plugins: [react()],
   server: {
-    port: 5000,
+    port: 3000,
     proxy: {
       "/api": {
         target: "http://localhost:5000",
