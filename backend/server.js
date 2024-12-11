bf118d6d82d920151413237daa7e5cec9e2f06e1
albaceo12 <h.nouri1999@gmail.com>
2024-12-11 22:23:12 +0330

    final stage completedgit add .

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import authRoutes from "../backend/routes/auth.routes.js";
 import messageRoutes from "../backend/routes/message.routes.js";
 import userRoutes from "../backend/routes/user.routes.js";
@@ -8,12 +9,18 @@ import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
 // const app = express();// we are setting socketio thats why we commenting this line
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+// console.log(__dirname);
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/users", userRoutes);
+app.use(express.static(path.join(__dirname, "/forntend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 server.listen(PORT, async () => {
   console.log(`server running on port ${PORT}!!!`);
   await connectTomongodb();
