f9c92fca9c289a24d583fd4f938cd896592aa914
albaceo12 <h.nouri1999@gmail.com>
2024-12-12 02:18:42 +0330

    final stage completedgit add .!

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  // base: "/",
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 5000,
     proxy: {
       "/api": {
         target: "http://localhost:5000",
