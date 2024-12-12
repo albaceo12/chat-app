260591d30f07bc2d5159f50a0518a120a61346fd
albaceo12 <h.nouri1999@gmail.com>
2024-12-12 20:20:13 +0330

    final stage completed

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,9 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/users", userRoutes);
-app.use(express.static(path.join(__dirname, "/forntend/dist")));
+app.use(express.static(path.join(__dirname, "/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 server.listen(PORT, async () => {
   console.log(`server running on port ${PORT}!!!`);
